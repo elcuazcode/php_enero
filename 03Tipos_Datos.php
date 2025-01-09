23ad4c38fc3d48917d405bbaf2622c1ddb4b3727
Jonathan <jonathanvelasquez073@gmail.com>
2025-01-08 20:59:35 -0600

    Third commint

---
+++ 03Tipos_Datos.php
@@ -0,0 +1,15 @@
+<?php
+
+// Tipo de Datos
+
+
+$nombre = "Erik";               // Dato String
+$edad = 40;                     // Entero
+$_promerdio = 1.9;              // Float
+$competitivo = True;            // Boolean
+
+
+echo "Su nombre es: ".$nombre." Su edad es: ".$edad." Su promerdio es: ".$_promerdio;
+
+
+?>
---
+++ 04Constantes.php
@@ -0,0 +1,16 @@
+<?php
+
+// Primera manera de crear una constante
+const madre = "Mariana";
+
+
+// Crear una constante utilizando una funcion
+define("padre", "Chon");
+
+
+
+$nombre_estudiante = "Jonny Ramon";
+
+echo "Estudiante ".$nombre_estudiante." su madre es ".madre." su padre es ".padre;
+
+?>
