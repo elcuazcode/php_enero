2f8c964643ff8e6589c3215a79b7cf9d00b40ded
Jonathan <jonathanvelasquez073@gmail.com>
2025-01-08 20:42:16 -0600

    Second commit

---
+++ 01HolaMundo.php
@@ -0,0 +1,13 @@
+<?php
+
+
+// print solo permite una cadena al mismo tiempo
+print "Hola mundo";
+
+// echo permite concatenar cadenas
+echo "Hola mundo ", "En cadena ", "Al infinito";
+
+
+// Primera leccion saber como abrir las etiquetas de php <?php ? >
+
+?>
---
+++ 02Variables_Constantes.php
@@ -0,0 +1,15 @@
+<?php
+
+$mi_nombre = "Juan";
+$_tu_nombre = "Reyes";
+$los_nombres = "Juan Reyes";
+
+// Utilizamos el . para concatenar, es la forma correcta de hacerlo
+echo " ".$mi_nombre." ".$mi_nombre;
+
+// Mientras la coma simplemente une valores, suenan similares pero no lo son.
+echo $mi_nombre, $mi_nombre;
+
+
+
+?>
