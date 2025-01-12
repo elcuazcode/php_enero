570d0099f325832b60cca151e22bbc0aa093270a
elcuazcode2 <jonathanvelasquez073@gmail.com>
2025-01-12 11:27:44 -0600

    send last change from my laptop

---
+++ 01Operadores.php
@@ -0,0 +1,21 @@
+<?php
+
+// Operadores
+// Operador + * - /
+// Operando 8 9 0 43 entidad sobre la que actua un operador
+
+// UNARIOS
+// $total ++
+// Operadondo Operador         
+
+// BINARIOS
+// $total + $iva
+// Operando Operador Operando
+
+// Ternarios
+// $condicion ?   'true' : 'false'
+// Operando Operador Operando Operador Operando
+
+
+
+?>
